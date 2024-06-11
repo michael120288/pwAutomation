8ef0c35c99400a332bda324c295581fc2f4d2b8d
michael120288 <Michael.sheptun@gmail.com>
2024-06-10 22:50:33 -0400

    iframe

---
+++ tests/popUp.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "@playwright/test";
+
+test.describe('POPUP WINDOW', () => {
+  test('popUp window accept', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+    page.on('dialog',el => el.accept())
+    await page.locator('#confirmbtn').click()
+  })
+  test('popUp window dismiss', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+    page.on('dialog',el => el.dismiss())
+    await page.locator('#confirmbtn').click()
+  })
+})
