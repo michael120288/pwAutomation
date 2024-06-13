0aeebf6bcfde6be81448234d44405def1e36e010
michael120288 <Michael.sheptun@gmail.com>
2024-06-13 15:28:18 -0400

    headless:true

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
 
       use: {
         ...devices["Desktop Chrome"],
-        headless: false,
+        headless: true,
         //storageState: "./.auth/user.json",
       },
       // dependencies: ["setup"],
